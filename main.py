7eed02251385d04268fa5e26cf53702a00c7e6b1
ZielaM <igor.zielinski@uczen.zsk.poznan.pl>
2024-11-18 17:05:56 +0100

    Simplified code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from collections.abc import Iterator
 import requests as req
 from bs4 import BeautifulSoup as bs, ResultSet, Tag
 import json
@@ -6,7 +5,7 @@ import json
 URL = 'https://www.zsk.poznan.pl/plany_lekcji/2023plany/technikum/plany/'
 WEEK = ['poniedzialek', 'wtorek', 'środa', 'czwartek', 'piątek']
 PLANY: dict[str, dict[str, list[str]]] = dict()
-PLAIN_TEXT = dict() #to be deleted
+PLAIN_TEXT: dict[str, dict[str, dict[str, str]]] = dict() 
 
 with open('./JSON/lessons.json', 'r', encoding='utf-8') as f:
     LESSONS: dict[str, str] = json.load(f)
@@ -33,17 +32,18 @@ def extract_data(lesson_title: str, lesson_teacher: str, lesson_classroom: str, 
 
 def main() -> None:
     for i in range(1, 32):
-        page: req.Response = req.get(f'{URL}o{i}.html')
-        soup: bs = bs(page.content, 'html.parser')
-        grade: str = soup.find('span', class_ = 'tytulnapis').text.split(' ')[0]
+        timetable = req.get(f'{URL}o{i}.html')
+        timetable_html = bs(timetable.content, 'html.parser')
+        grade = timetable_html.find('span', class_ = 'tytulnapis').text.split(' ')[0]
         print(grade)
-        num_row: int; row: Tag
-        for num_row, row in enumerate(soup.find('table', class_='tabela').find_all('tr')[1:]):
-            num_col: int; col: Tag
+        row: Tag
+        for num_row, row in enumerate(timetable_html.find('table', class_='tabela').find_all('tr')[1:]):
+            col: Tag
             for num_col, col in enumerate(row.find_all('td')[2:]):
                 col_spans: ResultSet[Tag] = col.find_all('span', recursive=False)
                 if len(col_spans) == 0:
-                    if col.text == '\xa0': continue
+                    if col.text == '\xa0': 
+                        continue
                     if grade not in PLAIN_TEXT:
                         PLAIN_TEXT[grade] = dict()
                     if num_col not in PLAIN_TEXT[grade]:
@@ -51,27 +51,23 @@ def main() -> None:
                     PLAIN_TEXT[grade][num_col][num_row] = col.text
                     
                     if col.text not in PLAIN_TEXT_SOLUTION:
-                        raise ValueError(f'Error: {col.text} not in PLAIN_TEXT_SOLUTION')
+                        raise ValueError(f'Error: {grade}/{WEEK[num_col]}/{num_row}: {col.text} not in PLAIN_TEXT_SOLUTION')
                     if PLAIN_TEXT_SOLUTION[col.text] is None:
                         continue
                     else:
-                        col_spans: list[str] = PLAIN_TEXT_SOLUTION[col.text].split('/')
-                        for span in col_spans:
+                        for span in PLAIN_TEXT_SOLUTION[col.text].split('/'):
                             extract_data(*span.split(' '), num_col, num_row, grade)
                     
-                    #to be added (plain text problem)
                 elif len(col_spans) == 3:
                     extract_data(*get_lesson_details(col_spans), num_col, num_row, grade)
                 elif len(col_spans) == 2:
-                    span: Tag
                     for span in col_spans:
                         extract_data(*get_lesson_details(span.find_all('span')), num_col, num_row, grade)
                 elif len(col_spans) == 1:
                     extract_data(*get_lesson_details(col_spans[0].find_all('span', recursive=False)), num_col, num_row, grade)
                 else:
                     it = iter(col_spans)
-                    col_spans = zip(it, it, it)
-                    for span in col_spans:
+                    for span in zip(it, it, it):
                         extract_data(*get_lesson_details(span), num_col, num_row, grade)
                     
 if __name__ == '__main__':

9e183d6f432064bdcd0149862ac940e3bc067f83
ZielaM <igor.zielinski@uczen.zsk.poznan.pl>
2025-01-17 15:50:36 +0100

    cleaned comments in utils.getting

--- utils/getting.py
+++ utils/getting.py
@@ -14,9 +14,9 @@ def get_number_of_timetables() -> int:
     Returns:
         int: number of timetables
     """
-    response = requests.get('https://www.zsk.poznan.pl/plany_lekcji/2023plany/technikum/lista.html')  # get the main page
-    soup = bs(response.text, 'html.parser')  # parse the main page
-    return len(soup.find_all('a')) - 1  # return the number of timetables (substract 1 because the first link is not a timetable)
+    response = requests.get('https://www.zsk.poznan.pl/plany_lekcji/2023plany/technikum/lista.html')  # get the page with timetables list
+    soup = bs(response.text, 'html.parser') 
+    return len(soup.find('table').find_all('a')) 
 
 
 def get_lesson_details(span: ResultSet[Tag], group: str) -> tuple[str, str, str, str|None]:
@@ -32,7 +32,7 @@ def get_lesson_details(span: ResultSet[Tag], group: str) -> tuple[str, str, str,
         tuple[str, str, str, str|None]: returns a tuple with lesson title,
         teacher, classroom and group number in string format
     """
-    if group is None and len(span[0].text.split('-')) == 2: 
+    if group is None and len(span[0].text.split('-')) == 2: # if group is None and there is a group in the lesson title, extract it
         span[0], group = span[0].text.split('-')
     lesson_title: str = w if (w := span[0].text.split('-')[0]) not in LESSONS else LESSONS[w]  # if lesson is corrupted, replace it with correct one
     lesson_teacher: str = w if (w := span[1].text)[0] != '#' else TEACHERS[w]  # if teacher is corrupted, replace it with correct one
@@ -41,43 +41,43 @@ def get_lesson_details(span: ResultSet[Tag], group: str) -> tuple[str, str, str,
 
 
 async def get_timetable(session: ClientSession, i: int, TIMETABLES: tuple, PLAIN_TEXT: dict) -> None:
-    async with session.get(f'{URL}o{i}.html') as response:  # get the timetable
-        timetable_html = bs(await response.text(), 'html.parser')  # parse the timetable
+    async with session.get(f'{URL}o{i}.html') as response: 
+        timetable_html = bs(await response.text(), 'html.parser') 
         grade = timetable_html.find('span', class_='tytulnapis').text.split(' ')[0]  # get the grade
         print(grade)  # print the grade so we know the progress
         row: Tag
         for num_row, row in enumerate(timetable_html.find('table', class_='tabela').find_all('tr')[1:]):  # iterate over the lesson numbers (rows)
             col: Tag
             for num_col, col in enumerate(row.find_all('td')[2:]):  # iterate over the days (columns)
-                col_spans: ResultSet[Tag] = col.find_all('span', recursive=False)  # get the spans from the column (the data is stored in spans, exept the plain text)
-                groups = [col.text[x:x+4] for x in [i for i, letter in enumerate(col.text) if letter == '-']] # get the groups from corrupted data (it's stored in plain text)
-                if len(col_spans) == 0:  # if there are no spans, it's a plain text
-                    if col.text == '\xa0':  # if the text is empty just skip it
+                col_spans: ResultSet[Tag] = col.find_all('span', recursive=False)  # get the data from the table cell
+                groups = [col.text[x:x+4] for x in [i for i, letter in enumerate(col.text) if letter == '-']] # get the groups from data
+                if len(col_spans) == 0:  # plain text case
+                    if col.text == '\xa0':  # skip if empty
                         continue
-                    if grade not in PLAIN_TEXT:  # if the grade is not in the PLAIN_TEXT dictionary, add it 
-                        # (it isn't used directly in the program, but it's useful for creating PLAIN_TEXT_SOLUTION)
+                    if grade not in PLAIN_TEXT:
                         PLAIN_TEXT[grade] = dict()
-                    if num_col not in PLAIN_TEXT[grade]:  # if the day is not in the PLAIN_TEXT dictionary, add it in number format (same as above)
+                    if num_col not in PLAIN_TEXT[grade]:
                         PLAIN_TEXT[grade][num_col] = dict()
-                    PLAIN_TEXT[grade][num_col][num_row] = col.text  # add the plain text to the PLAIN_TEXT dictionary 
+                    PLAIN_TEXT[grade][num_col][num_row] = col.text # add the plain text to PLAIN_TEXT 
 
-                    if col.text not in PLAIN_TEXT_SOLUTION:  # if the plain text is not in the PLAIN_TEXT_SOLUTION dictionary, raise an error (if so probably the file is outdated)
-                        raise ValueError(f'Error: {grade}/{num_col}/{num_row}: {col.text} not in PLAIN_TEXT_SOLUTION')
-                    if PLAIN_TEXT_SOLUTION[col.text] is None:  # if the plain text solution is None, skip it (I considered it an unnecessary data)
+                    if col.text not in PLAIN_TEXT_SOLUTION:  # print an error and continue in case of missing substitution
+                        print(f'Error: {grade}/{num_col}/{num_row}: {col.text} not in PLAIN_TEXT_SOLUTION')
                         continue
-                    else:  # if the plain text is in the PLAIN_TEXT_SOLUTION dictionary, iterate over the spans and put the data in the dictionaries
-                        for span in PLAIN_TEXT_SOLUTION[col.text].split('//'):
-                            group = span.split(' ')[0].split('-')[1] if len(span.split(' ')[0].split('-')) == 2 else None
-                            insert_all(*span.split(' '), group, num_col, num_row, grade, *TIMETABLES)  # insert the data to the dictionaries
+                    if PLAIN_TEXT_SOLUTION[col.text] is None:  # unnecessary data
+                        continue
+                    else: 
+                        for span in PLAIN_TEXT_SOLUTION[col.text].split('//'): # iterate over the lessons
+                            group = span.split(' ')[0].split('-')[1] if len(span.split(' ')[0].split('-')) == 2 else None # get the group from the lesson title
+                            insert_all(*span.split(' '), group, num_col, num_row, grade, *TIMETABLES) 
 
-                elif len(col_spans) == 3:  # if there are 3 spans, put the data in the Dictionaries (the default case)
+                elif len(col_spans) == 3:  # 1 lesson in the cell case
                     insert_all(*get_lesson_details(col_spans, groups[0] if len(groups) != 0 else None), num_col, num_row, grade, *TIMETABLES)
-                elif len(col_spans) == 2:  # if there are 2 spans, iterate over it and put the data in the Dictionaries (group lesson case)
+                elif len(col_spans) == 2:  # group lesson case
                     for k, span in enumerate(col_spans):
                         insert_all(*get_lesson_details(span.find_all('span'), groups[k] if len(groups) != 0 else None), num_col, num_row, grade, *TIMETABLES)
-                elif len(col_spans) == 1:   # if there is only one span, it's a group lesson with one group (half of the class case)
+                elif len(col_spans) == 1:   # group lesson case (half of the class)
                     insert_all(*get_lesson_details(col_spans[0].find_all('span', recursive=False), groups[0] if len(groups) != 0 else None), num_col, num_row, grade, *TIMETABLES)
-                else:  # if there are more than 3 spans, iterate over it, organize spans into groups of 3 and put the data in the Dictionaries (more than two groups case)
+                else:  # more than two groups case
                     it = iter(col_spans)
                     for k, span in enumerate(zip(it, it, it)):
                         insert_all(*get_lesson_details(span, groups[k] if len(groups) != 0 else None), num_col, num_row, grade, *TIMETABLES)
